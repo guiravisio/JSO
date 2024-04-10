e734fcc5547e29fc248883b888ad49ac5c6e2f57
Guilherme Ravisio <guilherme.ravisio@gmail.com>
2024-04-10 17:51:17 -0300

    Implementação do slider

--- static/script.js
+++ static/script.js
@@ -1,3 +1,4 @@
+
 /* Abre e fecha navegação */
 const menuMobile = document.querySelector('.menu-mobile')
 const body = document.querySelector('body')
@@ -16,7 +17,28 @@ navItem.forEach(item => {
     item.addEventListener("click", () =>{
         if(body.classList.contains("menu-nav-active")){
             body.classList.remove("menu-nav-active")
-            menuMobile.classList.replace("bi-x" , "bi-list")
+            menuMobile.classList.replace("bi-x" , "bi-list");
         }
     })
 })
+
+$('.slider-responsive').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      }
+    ]
+  });
